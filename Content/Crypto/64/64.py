56b04bfd9475415b96f49b7f69c7f5e7b0a04671
Andrew Kiryukhin <andrewkir2001@mail.ru>
2018-06-22 11:40:11 +0300

    change .txt to .md , fix 64.py

--- Content/Crypto/64/64.py
+++ Content/Crypto/64/64.py
@@ -5,9 +5,10 @@ data = sys.argv[1]
 code = sys.argv[2]
 
 binar = bytes(data, 'utf-8')
-decoded_data = base64.b64decode(binar)
-encoded_data = base64.b64encode(binar)
+
 if code == 'de':
+    decoded_data = base64.b64decode(binar)
     print(decoded_data.decode('utf-8'))
-elif code == 'en':
+else:
+    encoded_data = base64.b64encode(binar)
     print(encoded_data.decode('utf-8'))
